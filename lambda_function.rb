149f0a8450ce2e5d80edb6ed05de228d05efa27c
ujihisa <ujihisa@users.noreply.github.com>
2021-03-22 05:57:17 -0700

    Show more info

--- lambda_function.rb
+++ lambda_function.rb
@@ -3,11 +3,16 @@ require 'json'
 def lambda_handler(event:, context:)
   body = <<~EOS
     ｷﾀ━━━━(ﾟ∀ﾟ)━━━━!!
-    #{RUBY_DESCRIPTION}
+    #{{
+      RUBY_DESCRIPTION: RUBY_DESCRIPTION,
+      event: event,
+      context: context,
+    }.pretty_inspect}
   EOS
 
   {
     statusCode: 200,
-    body: JSON.generate(body),
+    # body: JSON.generate(body),
+    body: body,
   }
 end
